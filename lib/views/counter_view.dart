0cb441a3aaca7818ed33d05746410d89b9cf34a7
ali muhammad <you@example.com>
2025-08-24 16:49:46 +0500

    added snackbar

--- lib/views/counter_view.dart
+++ lib/views/counter_view.dart
@@ -1,51 +1,62 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:get/get_core/src/get_main.dart';
 import 'package:learn_getx/controller/counter_controller.dart';
 import 'package:learn_getx/home.dart';
 
+
 class CounterView extends StatelessWidget {
-   CounterView({super.key});
-CounterController counterController = Get.put(CounterController());
+  final CounterController counterController = Get.put(CounterController());
+
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-      // body: GetBuilder<CounterController>(
-      //   builder:(context){
-      //     return Center(
-      //       child: Text("$(countercontrol.count)")
-            
-      //     );
-      //   } ,),
- body: Obx(
-  () => Center(
-    child: Text(
-      "${counterController.count}",
-      style: TextStyle(fontSize: 40),
-    ),
-  ),
-),
-floatingActionButton: Row(
-  mainAxisAlignment: MainAxisAlignment.end,
-  children: [
-    FloatingActionButton(
-      heroTag: "btn1",
-      onPressed: () {
-        counterController.increaseCount();
-      },
-      child: Icon(Icons.add),
-    ),
-    SizedBox(width: 10),
-    FloatingActionButton(
-      heroTag: "btn2",
-      onPressed: () {
-        Get.to(Home()); 
-      },
-      child: Icon(Icons.home),
-    ),
-  ],
-),
+    return Scaffold(
+      appBar: AppBar(title: Text("Counter with GetX")),
+      body: Obx(
+        () => Center(
+          child: Text(
+            "${counterController.count}",
+            style: TextStyle(fontSize: 40),
+          ),
+        ),
+      ),
+      floatingActionButton: Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          FloatingActionButton(
+            heroTag: null,
+            onPressed: () {
+              counterController.increaseCount();
 
+              Get.snackbar(
+                "Counter Updated",                
+                "Value: ${counterController.count}", 
+                snackPosition: SnackPosition.BOTTOM,
+                backgroundColor: Colors.black87,
+                colorText: Colors.white,
+                margin: EdgeInsets.all(10),
+                duration: Duration(seconds: 2),
+              );
+            },
+            child: Icon(Icons.add),
+          ),
+          SizedBox(width: 10),
+          FloatingActionButton(
+            heroTag: null,
+            onPressed: () {
+              Get.to(Home());
+
+              Get.snackbar(
+                "Navigation",
+                "Moved to Home Screen",
+                snackPosition: SnackPosition.TOP,
+                backgroundColor: Colors.blueAccent,
+                colorText: Colors.white,
+              );
+            },
+            child: Icon(Icons.home),
+          ),
+        ],
+      ),
     );
   }
 }
